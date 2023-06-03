48a9eebeff39f622d95c2db16270e99cdf717734
Pablo Vitor <thevitor00@gmail.com>
2023-06-03 10:10:51 -0300

    Small progress on the overall project

--- js/script.js
+++ js/script.js
@@ -1,7 +1,31 @@
 const button = document.querySelector(".button-add-task");
 const input = document.querySelector(".input-task");
+const listaCompleta = document.querySelector(".list-task")
+
+let minhaListaDeItens = [];
+
+
 
 button.addEventListener('click', (e) => {
     e.preventDefault();
-    console.log(input.value);
-})
+    minhaListaDeItens.push(input.value);
+    input.value = ''
+    mostrarTarefas();
+
+    console.log(minhaListaDeItens);
+
+});
+
+function mostrarTarefas () {
+    let novaLi = ''
+    minhaListaDeItens.forEach(tarefa => {
+        novaLi = novaLi +  `
+        <li class="task">
+        <img src="./img/checked.png" alt="check-na-tarefa">
+        <p>${tarefa}</p>
+        <img src="./img/trash.png" alt="deletar-tarefa">
+    </li>`
+    });
+
+    listaCompleta.innerHTML = novaLi;
+}
